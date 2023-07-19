ae74359da5a0cb8bc92ea7ba9f7bb6f57e508d2a
Sasangi Nayanathara <sasanginayanathara@gmail.com>
2023-07-20 03:19:24 +0530

    Create common app bar

---
+++ lib/widgets/global/custom_app_bar.dart
@@ -0,0 +1,50 @@
+import 'package:ayura/constants/colors.dart';
+import 'package:ayura/constants/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+   final IconData leftIcon;
+   final IconData rightIcon;
+   final String title;
+   final Function? leftCallback;
+   CustomAppBar(this.leftIcon, this.rightIcon,this.title, {this.leftCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        // top: MediaQuery.of(context).padding.top,
+        top: 50,
+        left: 25,
+        right: 25,
+      ),
+      //appbar with title and back icon and righticon
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            onTap: leftCallback != null ? () => leftCallback!() : null,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Icon(leftIcon),
+            ),
+          ),
+          //middle title
+          Text(
+            title,
+            style: AppStyles.appBarTitleStyle,
+          ),
+          //right icon
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Icon(
+              rightIcon,
+              color: AppColors.primaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
