22fcd2ecd8e7bd0f648bb42aa5a2a6f6611a7c61
Sasangi Nayanathara <sasanginayanathara@gmail.com>
2023-07-20 19:08:30 +0530

    Complete mood chips with first page

--- lib/pages/features/mood_tracking/page1.dart
+++ lib/pages/features/mood_tracking/page1.dart
@@ -1,8 +1,13 @@
 import 'package:ayura/constants/styles.dart';
+import 'package:ayura/widgets/features/register/mood_traking/mood_chips.dart';
 import 'package:ayura/widgets/global/custom_app_bar.dart';
+import 'package:ayura/widgets/global/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ayura/pages/features/mood_tracking/page2.dart';
+// import 'package:ayura/widgets/features/mood_tracking/mood_chips.dart';
+
 
 class SelectMood extends StatelessWidget {
   const SelectMood({super.key});
@@ -15,32 +20,82 @@ class SelectMood extends StatelessWidget {
       ),
     );
     return Scaffold(
+      //top app bar with icons
+      appBar: CustomAppBar(Icons.arrow_back_outlined,Icons.calendar_month_outlined,"Mood Tracker"),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20,),
       
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //top app bar with icons
-          CustomAppBar(Icons.arrow_back_outlined,Icons.calendar_month_outlined,"Mood Tracker"),
-          const SizedBox(height: 20,),
-
-          //top image
-          Center(
-            child: SvgPicture.asset(
-              'assets/images/mood_tracker_pg1.svg',
-              width: 350,
-              height: 350,
+            //top image
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/mood_tracker_pg1.svg',
+                width: 350,
+                height: 350,
+              ),
             ),
-          ),
-          const SizedBox(height: 20,),
-          //middle question
-          const Center(child: Text(
-            "What was your mood today?",
-            style: AppStyles.mainQuestionsStyle,
+            const SizedBox(height: 20,),
+            //middle question
+            const Center(child: Text(
+              "What was your mood today?",
+              style: AppStyles.mainQuestionsStyle,
+              ),
             ),
-          ),
-          //moods chips
-
-        ],
+            const SizedBox(height: 20,),
+            //moods chips
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                child: Expanded(
+                  child:Wrap(
+                    spacing: 8.0,
+                    runSpacing: 8.0,
+                    children: [
+                      
+                       MoodButton(emojiName: 'happy', mood: 'Happy'),
+                        MoodButton(emojiName: 'tired', mood: 'Tired'),
+                        MoodButton(emojiName: 'sleepy', mood: 'Sleepy'),
+                        MoodButton(emojiName: 'furious', mood: 'Angry'),
+                        MoodButton(emojiName: 'bored', mood: 'Bored'),
+                        MoodButton(emojiName: 'excited', mood: 'Excited'),
+                        MoodButton(emojiName: 'sad', mood: 'Sad'),
+                        MoodButton(emojiName: 'embarrassed', mood: 'Embarassed'),
+                        MoodButton(emojiName: 'sick', mood: 'Sick'),
+                        MoodButton(emojiName: 'lonely', mood: 'Lonely'),
+                        MoodButton(emojiName: 'surprised', mood: 'Surprised'),
+                        MoodButton(emojiName: 'calm', mood: 'Calm'),
+                        MoodButton(emojiName: 'scared', mood: 'Scared'),
+                        MoodButton(emojiName: 'stressed', mood: 'Stressed'),
+                        MoodButton(emojiName: 'hurt', mood: 'Hurt'),
+                        MoodButton(emojiName: 'nervous', mood: 'Nervous'),
+                       
+                    ],
+                  ) ,
+                  ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Center(
+              child: customButton(
+                        tap: () {
+                          // SecondPage();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SecondPage()),
+                          );
+                        //  PageNavigator(context: context).nextPage(const Home());
+                        },
+                        icon: Icons.arrow_forward,
+                        text: 'This is how I feel',
+                        width: 200,
+                        height: 60,
+                        context: context),
+            ),
+            const SizedBox(height: 20,),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/features/register/mood_traking/mood_chips.dart
@@ -0,0 +1,59 @@
+import 'package:ayura/constants/colors.dart';
+import 'package:ayura/pages/features/mood_tracking/page2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+class MoodButton extends StatefulWidget {
+  final String emojiName;
+  final String mood;
+  const MoodButton({super.key,
+    required this.emojiName,
+    required this.mood,
+  });
+
+  @override
+  State<MoodButton> createState() => _MoodButtonState();
+}
+
+class _MoodButtonState extends State<MoodButton> {
+  bool isSelected = false;
+  int selectedCount = 0;
+
+  void _onChipSelected(bool selected) {
+    setState(() {
+      isSelected = selected;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ActionChip(
+      
+        onPressed: () {
+          _onChipSelected(!isSelected);
+          selectedCount++;
+        },
+
+    
+      label: Text(
+        widget.mood,
+        style: TextStyle(
+          color: isSelected? Colors.white : Colors.black,
+        ),
+      ),
+      labelPadding: const EdgeInsets.all(10.0),
+      clipBehavior: Clip.antiAlias,
+      backgroundColor: isSelected ? AppColors.primaryColor:Colors.white,
+      
+      shadowColor: AppColors.shadowColor,
+      elevation: isSelected ? 4 : 2,
+      avatar: SvgPicture.asset(
+        'assets/images/${widget.emojiName}.svg',
+          
+      ),
+      
+      
+    );
+  }
+}
