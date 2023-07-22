f85b1e8ff93a00f3ea3e64731f9276a5b157ce31
namadee <namadeeshakya1819@gmail.com>
2023-07-22 15:23:55 +0530

    adding meal plan navigation to home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:ayura/pages/features/mealPlan/meal_screen.dart';
 import 'package:ayura/pages/features/mood_tracking/page1.dart';
 import 'package:ayura/auth/login.dart';
 import 'package:ayura/provider/autProvider/authentication_provider.dart';
@@ -27,7 +28,7 @@ class _HomeState extends State<Home> {
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           backgroundColor: Colors.white,
           elevation: 0,
           leading: IconButton(
@@ -35,28 +36,32 @@ class _HomeState extends State<Home> {
               Icons.arrow_back_ios,
               color: Colors.black,
             ),
-            TextButton(
-                onPressed: () {
-                  print(birthdayController.text);
-                  print(convertDate(birthdayController.text));
-                },
-                child: Text('Save')
-            ),
-            //move to mood tracker
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>const SelectMood(),
-                  ),
-                );
-              },
-              child: const Text('Mood')
-            ),
-          ],
+            onPressed: () {},
+          ),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            children: [
+              //move to mood tracker
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SelectMood(),
+                      ),
+                    );
+                  },
+                  child: const Text('Mood')),
+
+              TextButton(
+                  onPressed: () {
+                    PageNavigator(context: context)
+                        .nextPage(const MealScreen());
+                  },
+                  child: const Text('Meal Plan')),
+            ],
+          ),
+        ));
   }
 }
