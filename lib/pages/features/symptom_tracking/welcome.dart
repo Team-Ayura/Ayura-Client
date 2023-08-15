fbc257f185a87e899938c77aeda3bfa6e8021287
Dilanga-Harshani <aadilangaharshani@gmail.com>
2023-08-15 14:47:11 +0530

    Adding position detectors

--- lib/pages/features/symptom_tracking/welcome.dart
+++ lib/pages/features/symptom_tracking/welcome.dart
@@ -3,59 +3,172 @@ import 'package:ayura/pages/features/symptom_tracking/page2.dart';
 import 'package:ayura/utils/router.dart';
 import 'package:flutter/material.dart';
 
+import '../../../constants/styles.dart';
+
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Symptom Tracker'),
-      //   leading: IconButton(
-      //     icon: const Icon(Icons.arrow_back),
-      //     onPressed: () {
-      //       Navigator.pop(context);
-      //     },
-      //   ),
-      //   actions: [
-      //     IconButton(
-      //       icon: const Icon(Icons.calendar_today),
-      //       onPressed: () {
-      //         // Navigator.push(context, MaterialPageRoute(builder: (context) => Page2()));
-      //         PageNavigator(context: context).nextPage(Page2());
-      //       },
-      //     ),
-      //   ],
-      // ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-IconButton(onPressed:(){PageNavigator(context: context).nextPage(const Page2());} , icon:Icon(Icons.calendar_month_outlined) ),
-            ],),
+                IconButton(onPressed:(){
+                  PageNavigator(context: context).nextPage(const Page2());} ,
+                    icon:Icon(Icons.calendar_month_outlined) ),
+              ],
+          ),
           
           const Padding(
             padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
             child: Text(
               'Choose where you had an uncomfortable sensation today',
-              style: TextStyle(fontSize: 24.0),
+              style: AppStyles.headingTextStyle,
             ),
           ),
+          const SizedBox(height: 20.0),
           Expanded(
-            child: Center(
-              child: GestureDetector(
-                onTap: () {
-                  PageNavigator(context: context).nextPage(Page1());
-                },
-                child: Image.asset(
-                  'assets/images/symptom_tracking/human.png',
-                  width: 210,
-                  fit: BoxFit.contain,
-                ),
+              child: Stack(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/images/symptom_tracking/human.png',
+                      width: 210,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Positioned(
+                    left: 195,
+                    top: 15,
+                    child: GestureDetector(
+                      onTap: () {
+                        PageNavigator(context: context).nextPage(const Page1());
+                        print('Tapped on Head');
+                      },
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 150,
+                    top: 95,
+                    child: GestureDetector(
+                      onTap: () {
+                        PageNavigator(context: context).nextPage(const Page1());
+                        print('Tapped on Shoulder');
+                      },
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 130,
+                    top: 190,
+                    child: GestureDetector(
+                      onTap: () {
+                        PageNavigator(context: context).nextPage(const Page1());
+                        print('Tapped on Arm');
+                      },
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 195,
+                    top: 120,
+                    child: GestureDetector(
+                      onTap: () {
+                        PageNavigator(context: context).nextPage(const Page1());
+                        print('Tapped on Chest');
+                      },
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 195,
+                    top: 190,
+                    child: GestureDetector(
+                      onTap: () {
+                        PageNavigator(context: context).nextPage(const Page1());
+                        print('Tapped on Stomach');
+                      },
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 175,
+                    top: 325,
+                    child: GestureDetector(
+                      onTap: () {
+                        PageNavigator(context: context).nextPage(const Page1());
+                        print('Tapped on Knee');
+                      },
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 180,
+                    top: 460,
+                    child: GestureDetector(
+                      onTap: () {
+                        PageNavigator(context: context).nextPage(const Page1());
+                        print('Tapped on Knee');
+                      },
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
           ),
         ],
       ),
